3509804fd1d6396e0662bbb55d9a15714b2dc563
Domogo <domsatrapa@gmail.com>
2023-04-20 21:47:21 +0200

    from into

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,30 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let mut split_option = s.split_once(",");
+        if split_option.is_none() {
+            return Person::default();
+        }
+        let split = split_option.unwrap();
+        let name = split.0;
+        if name.len() == 0 {
+            return Person::default();
+        }
+
+        let age = split.1.parse::<usize>();
+        match age {
+            Ok(age) => Person {
+                name: String::from(name),
+                age: age,
+            },
+            Err(_) => Person::default(),
+        }
     }
 }
 
